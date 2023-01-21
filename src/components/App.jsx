afcc670731a0413ed27d179e3fc09877c6c292c8
ermz <eramirez4369@gmail.com>
2023-01-20 19:52:46 -0500

    Finished layout for filmpire

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,37 @@
 import React from 'react';
-import { CssBaseline } from '@mui/material';
-// import { Route, Switch } from 'react-router-dom';
+import { collapseClasses, CssBaseline } from '@mui/material';
+import { Route, Switch } from 'react-router-dom';
 
-const App = () => (
-  <div>
-    <CssBaseline />
-    <h1>Hello, World - Filmpire </h1>
-  </div>
-);
+import useStyles from './styles';
+
+import { Actors, MovieInformation, Movies, NavBar, Profile } from '.';
+
+const App = () => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <NavBar />
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Switch>
+          <Route exact path="/movie/:id">
+            <MovieInformation />
+          </Route>
+          <Route exact path="/actors/:id">
+            <Actors />
+          </Route>
+          <Route exact path="/">
+            <Movies />
+          </Route>
+          <Route exact path="/profiles/:id">
+            <Profile />
+          </Route>
+        </Switch>
+      </main>
+    </div>
+  );
+};
 
 export default App;
