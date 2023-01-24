44e6ae8e0adb3110a94bae4769f258dac32fb0f4
ermz <eramirez4369@gmail.com>
2023-01-23 23:28:33 -0500

    Finished setting up Redux

--- src/services/TMDB.js
+++ src/services/TMDB.js
@@ -1,6 +1,20 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
+const tmdbApiKey = process.env.REACT_APP_TMDB_KEY;
+const page = 1;
+// https://api.themoviedb.org/3/movie/{movie_id}?api_key=<<api_key>>&language=en-US
+
 export const tmdbApi = createApi({
   reducerPath: 'tmdbApi',
-  baseQuery: fetchBaseQuery({ baseUrl: '' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://api.themoviedb.org/3' }),
+  endpoints: (builder) => ({
+    //* Get Movies by [Type]
+    getMovies: builder.query({
+      query: () => `movie/popular?page=${page}&api_key=${tmdbApiKey}`,
+    }),
+  }),
 });
+
+export const {
+  useGetMoviesQuery,
+} = tmdbApi;
