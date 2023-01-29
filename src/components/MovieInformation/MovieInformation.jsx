a07af0a1ca1aad8036bddbf8f7d30b738ad2df13
ermz <eramirez4369@gmail.com>
2023-01-28 20:25:42 -0500

    Continue fleshing out Movie Information component

--- src/components/MovieInformation/MovieInformation.jsx
+++ src/components/MovieInformation/MovieInformation.jsx
@@ -6,9 +6,13 @@ import { useDispatch, useSelector } from 'react-redux';
 import axios from 'axios';
 import makeStyles from './styles';
 
+import { selectGenreOrCategory } from '../../features/currentGenreOrCategory';
+
 import { useGetMovieQuery } from '../../services/TMDB';
+import genreIcons from '../../assets/genres';
 
 const MovieInformation = () => {
+  const dispatch = useDispatch();
   const classes = makeStyles();
   const { id } = useParams();
   const { data, isFetching, error } = useGetMovieQuery(id);
@@ -48,7 +52,56 @@ const MovieInformation = () => {
         <Grid item className={classes.containerSpaceAround}>
           <Box display="flex" align="center">
             <Rating readOnly value={data.vote_average / 2} />
+            <Typography variant="subtitle1" gutterBottom style={{ marginLeft: '10px' }}>
+              {Math.round(data?.vote_average * 10) / 10} / 10
+            </Typography>
           </Box>
+          <Typography variant="h6" align="center" gutterBottom>
+            {data?.runtime}min {data?.spoken_languages.length > 0 ? `/ ${data?.spoken_languages[0].name}` : ''}
+          </Typography>
+        </Grid>
+        <Grid item className={classes.genresContainer}>
+          {data?.genres?.map((genre, i) => (
+            <Link
+              key={genre.name}
+              className={classes.links}
+              to="/"
+              onClick={() => dispatch(selectGenreOrCategory(genre.id))}
+            >
+              <img src={genreIcons[genre.name.toLowerCase()]} className={classes.genreImage} height={30} />
+              <Typography color="textPrimary" variant="subtitle1">
+                {genre?.name}
+              </Typography>
+            </Link>
+          ))}
+        </Grid>
+        <Typography variant="h5" gutterBottom style={{ marginTop: '10px' }}>
+          Overview
+        </Typography>
+        <Typography style={{ marginBottom: '2rem' }}>
+          {data?.overview}
+        </Typography>
+        <Typography variant="h5" gutterBottom>Top Cast</Typography>
+        <Grid item container spacing={2}>
+          {data && data.credits?.cast?.map((character, i) => (
+            character.profile_path && (
+            <Grid key={i} item xs={4} md={2} component={Link} to={`/actors/${character.id}`} style={{ textDecoration: 'none' }}>
+              <img className={classes.castImage} src={`https://image.tmdb.org/t/p/w500/${character.profile_path}`} alt={character.name} />
+              <Typography color="textPrimary">{character?.name}</Typography>
+              <Typography color="textSecondary">{character.character.split('/')[0]}</Typography>
+            </Grid>
+            )
+          )).slice(0, 6)}
+        </Grid>
+        <Grid item container style={{ marginTop: '2rem' }}>
+          <div className={classes.buttonsContainer}>
+            <Grid item xs={12} sm={6} className={classes.buttonsContainer}>
+              <ButtonGroup size="small" variant="outlined">
+                <Button target="_blank" rel="noopener noreferrer" href={data?.homepage} endIcon={<Language />}>Website</Button>
+                <Button target="_blank" rel="noopener noreferrer" href={`https://www.imdb.com/title/${data?.imdb_id}`} endIcon={<MovieIcon />}>IMDB</Button>
+              </ButtonGroup>
+            </Grid>
+          </div>
         </Grid>
       </Grid>
     </Grid>
